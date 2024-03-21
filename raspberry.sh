54c1ce4c00270a7b4bb73b1e63d9bda7cd17b289
alessiogandelli <alessiogandelli99@gmail.com>
2024-03-21 23:44:19 +0100

    setup raspberry

---
+++ raspberry.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+# oh-my-zsh
+echo "Installing oh-my-zsh ..."
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Setting up oh-my-zsh ..."
+# theme
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# config
+sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
+sed -i 's/^plugins=.*/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/' ~/.zshrc
+
+
+echo "Installing zerotier ..."
+curl -s https://install.zerotier.com | sudo bash
